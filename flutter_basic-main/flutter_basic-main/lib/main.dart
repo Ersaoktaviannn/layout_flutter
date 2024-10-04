b22972c0051de52a1280f7c4bafc7c73b8306752
ersaoktavian <ersaoktavianramadan@gmail.com>
2024-10-04 08:31:13 +0700

    first commit

---
+++ flutter_basic-main/flutter_basic-main/lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/basic_widgets/dialog_widget.dart';
+import 'package:flutter_basic/basic_widgets/fab_widget.dart';
+import 'package:flutter_basic/basic_widgets/image_widget.dart';
+import 'package:flutter_basic/basic_widgets/input_widget.dart';
+import 'package:flutter_basic/basic_widgets/loading_cupertino.dart';
+import 'package:flutter_basic/basic_widgets/text_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text('Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey)),
+            ],
+          ),
+        ),
+        /* soal 3*/
+        const Icon(
+          Icons.star_rate,
+          color: Colors.red,
+        ),
+        const Text("41"),
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Gunung Bromo atau dalam bahasa Tengger dieja "Brama", juga disebut Kaldera Tengger, adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.614 meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. 🙂.'
+        'Ersa Oktavian Ramadan-2241720208',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout: Nama dan NIM Anda',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/bromo.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
